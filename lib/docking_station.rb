81518fd60798580c88c9c800f5815255609116fd
Michael Nguyen <michaelnguyen974@gmail.com>
2018-11-28 15:31:46 +0000

    release bike test, working bike test, return true from working? method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,14 @@
+require_relative 'bike'
+
 class Dockingstation
 
   def release_bike
-
+  	bike = Bike.new
   end
 
 end
 
 docking_station = Dockingstation.new
 bike = docking_station.release_bike
+bike.working?
 
-#I am on step 9
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,14 @@ require "docking_station"
 
 describe Dockingstation do
   it { is_expected.to respond_to :release_bike }
+
+  it "releases the bike" do
+  	bike = Bike.new
+  	expect(bike).to be_working
+  end
+
+  # it { is_expected.to respond_to :dock }
+  	
+	# it "dock our bike at the docking station" do
+   # end
 end
